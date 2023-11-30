5dcd4b7215c1a10cb79807ccc19cf98eb56bbc38
rakudaBombayer <113893608+rakudaBombayer@users.noreply.github.com>
2023-11-30 23:07:05 +0900

    アイコンを入れました。

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import TwitterIcon from '@mui/icons-material/Twitter';
 
 function Sidebar() {
   return (
     <div className="sidebar">
       {/* ツイッターアイコン */}
+      <TwitterIcon />
 
       {/* Sidebaroption */}
       {/* Sidebaroption */}
