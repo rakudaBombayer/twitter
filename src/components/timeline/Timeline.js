2838c237abe378b3d59a6a83b774c5e3ed173d83
rakudaBombayer <113893608+rakudaBombayer@users.noreply.github.com>
2023-12-01 18:01:32 +0900

    TimeLineのホームを実装

--- src/components/timeline/Timeline.js
+++ src/components/timeline/Timeline.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import './Timeline.css'
+import TweetBox from './TweetBox'
 
 function Timeline() {
   return (
@@ -8,7 +10,8 @@ function Timeline() {
           <h2>ホーム</h2>
         </div>
       {/* TweetBox */}
-
+      <TweetBox />
+      
       {/* Post */}
     </div>
   )
