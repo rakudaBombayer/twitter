58acbd894ad02b39ef87ebbfeac9f9ecf7e6d99a
rakudaBombayer <113893608+rakudaBombayer@users.noreply.github.com>
2023-12-01 18:19:14 +0900

    ツイートボタン挿入

--- src/components/timeline/TweetBox.js
+++ src/components/timeline/TweetBox.js
@@ -1,9 +1,20 @@
+import { Avatar, Button } from '@mui/material'
 import React from 'react'
 
 function TweetBox() {
   return (
-    <div>
-      
+    <div className="tweetBox">
+      <form>
+        <Avatar />
+        <input 
+          placeholder="いまどうしてる？" type="text"></input>
+        <input 
+          className="tweetBox--imageInput"
+          placeholder="画像のURLを入力してください"
+          type="text"
+          ></input>
+          <Button className="tweetBox--tweetButton" type="submit">ツイートする</Button>
+        </form>
     </div>
   )
 }
