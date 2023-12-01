45f181cc2834a1f850399a73ab43c6d2e5fbb065
rakudaBombayer <113893608+rakudaBombayer@users.noreply.github.com>
2023-12-01 21:28:55 +0900

    ツイート投稿部分作成

--- src/components/timeline/TweetBox.js
+++ src/components/timeline/TweetBox.js
@@ -1,13 +1,16 @@
 import { Avatar, Button } from '@mui/material'
 import React from 'react'
+import './TweetBox.css'
 
 function TweetBox() {
   return (
     <div className="tweetBox">
       <form>
+        <div className="tweetBox--input">
         <Avatar />
         <input 
           placeholder="いまどうしてる？" type="text"></input>
+        </div>
         <input 
           className="tweetBox--imageInput"
           placeholder="画像のURLを入力してください"
